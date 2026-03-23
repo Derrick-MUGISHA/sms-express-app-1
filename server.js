e6096c315c388aa487289d96998e116f54d1a867
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2026-03-23 04:56:05 +0200

    removed the icons

--- server.js
+++ server.js
@@ -38,5 +38,5 @@ swaggerDocs(app);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
