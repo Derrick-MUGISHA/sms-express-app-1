b4399ec93b25b29a775ccfb28417057f4c87e5e5
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2026-06-29 10:21:47 +0200

    fixed the IPV6

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
   port: process.env.SMTP_PORT || 587,
   secure: process.env.SMTP_SECURE === 'true',
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
