ed7fbc236b9585a7ba741281525dfe48a69441a4
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2026-06-29 11:30:45 +0200

    fixed the supervisour

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,13 +1,9 @@
 const nodemailer = require('nodemailer');
-const dns = require('dns');
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
   port: process.env.SMTP_PORT || 587,
   secure: process.env.SMTP_SECURE === 'true',
-  lookup: (hostname, options, callback) => {
-    dns.lookup(hostname, { ...options, family: 4 }, callback);
-  },
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
