d76aba88ba527eef55b69699d196fea88de29320
Derrick-MUGISHA <derrickmugisha169@gmail.com>
2026-07-02 09:26:35 +0200

    reduced the rate limiture

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.use(helmet({
 
 // Global Rate Limiter
 const globalLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per window
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 10000, // Limit each IP to 100 requests per window
   message: { error: 'Too many requests, please try again later.' },
   standardHeaders: true,
   legacyHeaders: false,
